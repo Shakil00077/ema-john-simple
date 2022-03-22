9f78e7abef051e9c84afca3e34d6b415b598a357
Shakil00077 <shakilshehjaad@gmail.com>
2022-03-22 14:08:54 +0600

    Cart item starckture ready

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Product.css'
 
-const Product = (porps) => {
-    const {img, name, seller, price, ratings} = porps.product
+const Product = (props) => {
+    const { img, name, seller, price, ratings } = props.product;
+    const { handleAddToCart, product } = props;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -10,7 +11,7 @@ const Product = (porps) => {
             <p className='product-price'>Price: {price}</p>
             <p className='product-seller'>Manufacturer: {seller}</p>
             <p className='product-ratings'>Ratings: {ratings} star</p>
-            <button className='btn-cart'>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
                 <p>Add to Cart</p>
             </button>
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,12 +5,22 @@ import './Shop.css'
 const Shop = () => {
 
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data));
     }, [])
+
+
+    const handleAddToCart = (product) =>{
+        console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+
+    }
+
 
     return (
         <div className='shop-container'>
@@ -19,11 +29,18 @@ const Shop = () => {
                     products.map(product => <Product 
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                         ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h4>Order Summery</h4>
+                <h3 className='cart-order'>Order Summary</h3>
+                <div className='cart-info'>
+                <h5>Selected Items: {cart.length}</h5>
+                <p>Total Price:</p>
+                <p>Total Shipping Charge:</p>
+                <p>Tax:</p>
+                </div>
             </div>
         </div>
     );
