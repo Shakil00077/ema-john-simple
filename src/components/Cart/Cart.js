876e6215d456472cf59ff983cd7259b247c6435d
Shakil00077 <shakilshehjaad@gmail.com>
2022-03-23 12:30:43 +0600

    cart componend product management complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,28 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = (props) => {
+    const { cart } = props;
+    console.log(cart);
+
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 10 / 100).toFixed(2));
+    const grandTotal = total + shipping + tax;
+
     return (
-        <div>
+        <div className='cart'>
             <h3 className='cart-order'>Order Summary</h3>
-            <h5>Selected Items: {props.cart.length}</h5>
+            <div className='cart-info'>
+                <h5>Selected Items: {cart.length}</h5>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping: ${shipping}</p>
+                <p>Tax: ${tax}</p>
+                <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+            </div>
         </div>
     );
 };
